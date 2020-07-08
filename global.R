f3f0a378c96c39108bf555cd29e6ab0709ca49d7
elmerfer <elmerfer@gmail.com>
2020-07-08 18:41:01 -0300

    Update global.R

--- global.R
+++ global.R
@@ -1,6 +1,7 @@
 ##The global.R script
-load("Data/LM22.Rdata")
-load("Data/TIL10.Rdata")
+load("Data/LM22.RData")
+load("Data/TIL10.RData")
 library(parallel)
 library(ggplot2)
+library(openxlsx)
 .num.cores <- parallel::detectCores(all.tests = FALSE, logical = TRUE)
